19981672ba05493661674a778d04a59bf5dbda6c
Rapidement <contact@doriangrasset.fr>
2022-10-05 16:46:39 +0200

    initial

--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,21 @@
 const wrapperMenu = document.querySelector('.wrapper-menu');
 const menu = document.getElementById('menu');
-wrapperMenu.addEventListener('click', function(){
+
+let opened = false;
+const c = () => {
+    opened = !opened
     menu.classList.toggle('flex-row');
     menu.classList.toggle('flex-column');
     menu.classList.toggle('hidden');   
     menu.classList.toggle('opened'); 
     wrapperMenu.classList.toggle('open');
     document.body.classList.toggle('bodypadding')
-})
+}
+wrapperMenu.addEventListener('click', c)
 
 window.addEventListener('resize', function(){
     if (window.innerWidth > 768) {
+        if (opened) c();
         menu.classList.add('hidden');
         menu.classList.add('flex-row');
         menu.classList.remove('flex-column');
